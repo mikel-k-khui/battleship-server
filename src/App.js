f44549dbe38808648bcbc1c00fbcb6d53b01809d
Mike Chui <mikel.k.khui@gmail.com>
2019-10-03 16:15:32 -0600

    UpUpdated the sockets to send shots from test page and received test shots

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ var io = require('socket.io')(server);
 const ENV = process.env.NODE_ENV || "development"; 
 const PORT = process.env.PORT || 8001;
 const playerList = {};
+// const api_id = {};
 
 app.use(express.static(path.join(__dirname, '../public')));
 
@@ -15,10 +16,10 @@ app.use(express.static(path.join(__dirname, '../public')));
 //   console.log("In API server.");
 // });
 
-io.on('connect', (socket) => {
+const socket = io.on('connect', (socket) => {
   console.log((new Date().toISOString()) + ' ID ' + socket.id);
 
-  socket.on('player', (socketID, fn) => {
+  socket.on('player', (socketId, cb) => {
     //check socketID = socket.id
     playerList[socket.id] = {
       player_id: socket.id,
@@ -26,22 +27,30 @@ io.on('connect', (socket) => {
       ships: null,
       turn: null
     };
-    console.log("Confirmed player from client in server");
+    console.log("Confirmed player from client in server's id:", socket.id, " and client's id:", socketId);
 
-    return initiateGame(socket.id);
+    cb({ server: socket.id, client: socketId });
   });
 
-  socket.on('gameFeed', (data) => {
+  socket.on('gameFeed', (data, cb) => {
     console.log("Received new gameState from client in server:", data);
     //if right player's turn
     //update the board
     //ask for a move
     //send shot with full game data
+    cb('Testing emit-on');
   });
 
-  socket.on('shotFeed', (feed) => {
-    sendShot(feed);
+  // socket.on('api', (feed) => {
+  //   api_id['api'] = feed;
+  // });
+
+  // listener only for API
+  socket.on('shotFeed', (feed, cb) => {
+    console.log("Before send shot:", feed);
+    sendShot(feed, socket);
     console.log("Received feed from test page in server:", feed);
+    cb('sample');
   });
 
 
@@ -53,10 +62,13 @@ function getShot(socketID) {
 };
 
 // Sending a shot to the client
-function sendShot(target) {
-  socket.emit('serverFeed', target, (req, res) => {
-    console.log("Does emit has a callback?", req, " and ", res);
-  });
+function sendShot(target, aSocket) {
+  socket.emit('serverFeed', target);
+  console.log("Sent to", aSocket.id);
+
+  // , (req, res) => {
+  //   console.log("Does emit has a callback?", req, " and ", res);
+  // });
 };
 
 server.listen(PORT, () => {
